23af036e5fd8d53368bd19295889396abdb6ab77
nomaneeehstu <nomanhstu18@gmail.com>
2021-09-02 12:18:30 +0600

    aray-loop-js

---
+++ array-loop.js
@@ -0,0 +1,18 @@
+// var  numbers = [45, 56, 67, 78, 89, 87, 75];
+
+// for(var i=0; i<7; i++){
+//     var element = numbers[i];
+//     console.log(element);
+// // }
+// numbers.push(84);
+// for(var i=0; i<numbers.length; i++){
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+var items = ['bottle', 'mobile', 'charger', 'watch', 'book', 'paper'];
+
+for(var i=0; i<items.length; i++){
+    itemsShow = items[i];
+    console.log(itemsShow);
+}
